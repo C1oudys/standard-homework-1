a9dcc7fe3982a01a6c6257bdb248ac34e8c482fd
C1oudys <hymin310@gmail.com>
2024-01-26 19:47:14 +0900

    완료

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
 // TODO: FilterButtons 컴포넌트를 작성하세요. 필터링 옵션을 선택하는 버튼들을 포함해야 합니다.
-function FilterButtons({setStudents, setFilteredStudents}) { 
+function FilterButtons({initialStudents, setFilteredStudents}) { 
   const filterByAge = (minAge) => {
-    const filtered = students.fiter((student) => student.age >= minAge)
+    const filtered = initialStudents.filter((student) => student.age >= minAge)
     setFilteredStudents(filtered);
   };
   const filterByGrade = (grade) => {
-    const filtered = students.fiter((student) => student.grade >= grade)
+    const filtered = initialStudents.filter((student) => student.grade === grade)
     setFilteredStudents(filtered);
   };
   const resetFilter = () => {
-    setFilteredStudents(students);
+    setFilteredStudents(initialStudents);
   };
 
   return (
     <div>
       {/* 여기에 필터링 버튼들을 완성하세요. */}
-      <button onClick={() => filterByAge}>18세 이상</button>
-      <button onClick={() => filterByGrade}>A등급</button>
-      <button onClick={() => resetFilter}>필터 초기화</button>
+      <button onClick={() => filterByAge(18)}>18세 이상</button>
+      <button onClick={() => filterByGrade("A")}>A등급</button>
+      <button onClick={resetFilter}>필터 초기화</button>
     </div>
   );
 }
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 // TODO: StudentList 컴포넌트를 작성하세요. props로 학생 목록을 받아와서 표시해야 합니다.
-function StudentList({students}) {
+function StudentList({filteredStudents}) {
   return (
     <ul>
       {/* 여기에 학생 목록을 표시하는 로직을 작성하세요. */}
-      {students.map((student, index) => (
-        <li key={stduent.index}>
+      {filteredStudents.map((student, index) => (
+        <li key={index}>
           {student.name} ({student.age}세) - {student.grade}등급
         </li>
       ))};
